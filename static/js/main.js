04d40c06c9fa1d29ea75fcbf54ea6d2155fde03d
Thomas Richards <thomas.richards.96@gmail.com>
2017-03-16 23:46:15 +0000

    Adds easter egg

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,9 @@
 clippy.load({ name: 'Clippy', path: 'js/agents/' }, function(agent) {
 	agent.show();
 });
+
+function setKonamiCode() {
+	let easterEgg = new Konami();
+	easterEgg.code = partyMode;
+	easterEgg.load();
+}
