bc165dd5d9d9efe6eaa18b971fcc15c6d54d6c5d
Thomas Richards <thomas.richards.96@gmail.com>
2017-03-22 01:07:15 +0000

    Adds backend psuedocode

--- main.js
+++ main.js
@@ -2,8 +2,25 @@ require options
 
 boot websocket server
 boot http(s) server
-boot player
+boot player //???
 
-export 
+//websocket._socket.remoteAddress
+map1 = ip -> (unum,nickname)
+map2 = ip -> ws
+
+export
+	map1
+	map2
 	new content manager
-	new queue
+	new banlist
+	
+	if a queue is serialised
+		new playqueue from file
+	else
+		new playqueue
+
+on ctrl+D
+	end current song
+
+on ctrl+C
+	store current queue
