1e4e3a1876fb8c9a53c376699aba8ae7ab5c9cba
Thomas Richards <thomas.richards.96@gmail.com>
2017-04-10 22:51:17 +0100

    Finishes initial parse of content manager

--- options.js
+++ options.js
@@ -1,12 +1,12 @@
 module.exports = {
 	password: null,
-	buckets: 4,
 	timeout: 547,
+	maxQueuedAtOnce: 4,
 	storageDir: '/tmp/The-Music-o-Matic-2000/',
 	websocketPort: 8080,
 };
 
-//password   = the admin password, null gives anyone access
-//buckets    = the maximum number of tracks queued by each user
-//timeout    = the maximum number of seconds a song can play for
-//sotrageDir = the location on disk where uploaded songs are stored
+//password         = the admin password, null gives anyone access
+//maxQueuedAtOnce = the maximum number of tracks queued by each user
+//timeout          = the maximum number of seconds a song can play for
+//storageDir       = the location on disk where uploaded songs are stored
