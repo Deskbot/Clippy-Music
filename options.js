ac4155c470d2e543e9323c5e214f21381e974d49
Thomas Richards <thomas.richards.96@gmail.com>
2017-04-18 20:12:05 +0100

    Adds picture downloader

--- options.js
+++ options.js
@@ -4,9 +4,11 @@ module.exports = {
 	maxQueuedAtOnce: 4,
 	storageDir: '/tmp/The-Music-o-Matic-2000/',
 	websocketPort: 8080,
+	imageSizeLimit: 500000,
 };
 
 //password         = the admin password, null gives anyone access
-//maxQueuedAtOnce = the maximum number of tracks queued by each user
+//maxQueuedAtOnce  = the maximum number of tracks queued by each user
 //timeout          = the maximum number of seconds a song can play for
 //storageDir       = the location on disk where uploaded songs are stored
+//imageSizeLimit   = largest image that can be downloaded
