2ddf620609854a839032e7ff33e6f40b9c3facd7
Thomas Richards <thomas.richards.96@gmail.com>
2017-04-20 00:07:38 +0100

    Adds music size option

--- options.js
+++ options.js
@@ -4,11 +4,13 @@ module.exports = {
 	maxQueuedAtOnce: 4,
 	storageDir: '/tmp/The-Music-o-Matic-2000/',
 	websocketPort: 8080,
-	imageSizeLimit: 500000,
+	imageSizeLimit: 5000000,
+	musicSizeLimit: 20000000,
 };
 
 //password         = the admin password, null gives anyone access
 //maxQueuedAtOnce  = the maximum number of tracks queued by each user
 //timeout          = the maximum number of seconds a song can play for
 //storageDir       = the location on disk where uploaded songs are stored
-//imageSizeLimit   = largest image that can be downloaded
+//imageSizeLimit   = largest image that can be downloaded, locally or from the internet
+//musicSizeLimit   = largest image that can be downloaded locally
