d1856701a7cb267e9d30e3f18273853f8cb07ba8
Thomas Richards <thomas.richards.96@gmail.com>
2017-05-28 18:05:55 +0100

    Promise for clippy loading

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,19 @@
-clippy.load({ name: 'Clippy', path: 'js/agents/' }, function(agent) {
-	agent.show();
-});
+clippy.Balloon.prototype.CLOSE_BALLOON_DELAY = 20000;
+
+var clippyAgent = null;
+
+function isYouTubeUrl(url) {
+	return url.includes('youtube.com/watch')
+		|| url.includes('youtu.be');
+}
+
+function loadClippy() {
+	return new Promise(function(resolve, reject) {
+		clippy.load({ name: 'Clippy', path: 'js/agents/' }, (agent) => {
+			resolve(agent);
+		});
+	});
+}
 
 function setKonamiCode() {
 	let easterEgg = new Konami();
