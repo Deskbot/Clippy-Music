6668e1cd7d3e673cfcaed24bf193402016e001c2
Thomas Richards <thomas.richards.96@gmail.com>
2017-06-17 19:29:16 +0100

    Adds cooloff after which you can play content again

--- options.js
+++ options.js
@@ -5,11 +5,13 @@ module.exports = {
 	websocketPort: 8080,
 	imageSizeLimit: 5000000,
 	musicSizeLimit: 20000000,
+	uniquenessCoolOff: 86400,
 };
 
-//password         = the admin password, null gives anyone access
-//maxQueuedAtOnce  = the maximum number of tracks queued by each user
-//timeout          = the maximum number of seconds a song can play for
-//storageDir       = the location on disk where uploaded songs are stored, if a relative path is given, the directory will be relative to where the process was ran
-//imageSizeLimit   = largest image that can be downloaded, locally or from the internet
-//musicSizeLimit   = largest image that can be downloaded locally
+//password          = the admin password, null gives anyone access
+//maxQueuedAtOnce   = the maximum number of tracks queued by each user
+//timeout           = the maximum number of seconds a song can play for
+//storageDir        = the location on disk where uploaded songs are stored, if a relative path is given, the directory will be relative to where the process was ran
+//imageSizeLimit    = largest image that can be downloaded, locally or from the internet
+//musicSizeLimit    = largest image that can be downloaded locally
+//uniquenessCoolOff = how many seconds before it's ok to play the same music or show the same picture again
