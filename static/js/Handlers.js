4d397332c3f3f39b697ccc24fb63765e61da4d6e
Thomas Richards <thomas.richards.96@gmail.com>
2017-06-18 13:57:14 +0100

    Checks for bad input on front end

--- static/js/Handlers.js
+++ static/js/Handlers.js
@@ -2,7 +2,6 @@ Handlers = {};
 
 Handlers.set = function() {
 	$('#upload-form').submit(function() {
-		return true;
 		console.log("Handling upload form.");
 
 		var $this = $(this);
@@ -15,29 +14,12 @@ Handlers.set = function() {
 		if (musicUrl && !isYouTubeUrl(musicUrl)) {
 			clippyAgent.speak('Music URL given is not a YouTube link.');
 			return false;
-		} else {
-			itemTitle = musicUrl;
 		}
 
 		if (!musicFiles || musicFiles.length === 0) {
 			clippyAgent.speak('No music chosen for upload.'); //assumes check for url has happened
 			return false;
 
-		} else {
-			itemTitle = musicFiles[0].name;
 		}
-
-		var promise = submitForm($this);
-
-		promise.done(function(d, status, xhr) {
-			console.log(d, status, xhr);
-
-			clippyAgent.speak('Your item (\n' + stringWrap(itemTitle, 8, '\n') + ') has been added to the download queue.');
-			
-		}).fail(function(d, status, xhr){
-			console.log(d, status, xhr);
-		});
-
-		return false; //don't submit the form normally
 	});
 }
