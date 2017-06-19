f20bf4563db992119917b3a7d55fd1021ed2510a
Thomas Richards <thomas.richards.96@gmail.com>
2017-06-19 16:06:31 +0100

    Prevent XSS

--- options.js
+++ options.js
@@ -5,6 +5,7 @@ module.exports = {
 	websocketPort: 8080,
 	imageSizeLimit: 5000000,
 	musicSizeLimit: 20000000,
+	nicknameSizeLimit: 40,
 	uniquenessCoolOff: 86400,
 };
 
@@ -12,6 +13,7 @@ module.exports = {
 //maxQueuedAtOnce   = the maximum number of tracks queued by each user
 //timeout           = the maximum number of seconds a song can play for
 //storageDir        = the location on disk where uploaded songs are stored, if a relative path is given, the directory will be relative to where the process was ran
-//imageSizeLimit    = largest image that can be downloaded, locally or from the internet
-//musicSizeLimit    = largest image that can be downloaded locally
+//imageSizeLimit    = largest image that can be downloaded, locally or from the internet in bytes
+//musicSizeLimit    = largest image that can be downloaded locally in bytes
+//nicknameSizeLimit = maximum nickname length
 //uniquenessCoolOff = how many seconds before it's ok to play the same music or show the same picture again. Can use Infinity for the duration to never expire, or -Infinity to never check for uniqueness
