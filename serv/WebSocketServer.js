b68020098e2181f620c4d398236a76d11dcf0e1a
Thomas Richards <thomas.richards.96@gmail.com>
2017-09-09 18:49:19 +0100

    Makes content manager emit an event for each new item played
    The dependecy diagram had a problem because WebSocketServer and ContentManager need each other.
    I want the manager itself to be decoupled from the content server api, the same way I have done with websockets.

--- serv/WebSocketServer.js
+++ serv/WebSocketServer.js
@@ -4,57 +4,64 @@ const WebSocketHandler = require('../lib/WebSocketHandler.js');
 const ContentServer = require('./ContentServer.js');
 const UserRecServ = require('./UserRecordServer.js');
 
-
-const wsHandler = new WebSocketHandler(onConnect, onMessage, onClose);
-
-function onConnect(soc) {
-	const id = WebSocketHandler.socToUserId(soc);
-
-	//send queue
-	Api.sendQueue(soc);
-
-	//save user
-	UserRecServ.add(id);
-	UserRecServ.setWS(id, soc);
-
-	//notify if banned
-	if (UserRecServ.isBanned(id)) Api.sendBanned(soc);
-
-	//tell user their nickname
-	if (UserRecServ.isUser(id)) Api.sendNickname(soc, UserRecServ.getNickname(id));
-}
-
-function onMessage(soc, data) {
-	const dataObj = JSON.parse(data);
-
-	if (dataObj.type === 'delete-content') {
-		if (!ContentServer.remove(wsHandler.socToUserId(soc), dataObj.contentId)) {
-			soc.send(JSON.stringify({
-				type: dataObj.type,
-				success: false,
-				reason: 'The queue item you tried to remove was not chosen by you.',
-			}));
-		}
-
-	} else {
-		soc.send(JSON.stringify({
-			type: dataObj.type,
-			success: false,
-			reason: 'The server did not recognise the type of message you were trying to send.',
-		}));
-	}
-}
-
-function onClose(soc, id) {
-	UserRecServ.unsetWS(id, soc);
-}
-
 //really a namespace where all functions are hoisted
 class Api {
 
+	constructor() {
+		this.wsh = new WebSocketHandler(onConnect.bind(this), onMessage.bind(this), onClose.bind(this));
+
+		ContentServer.on('item', function() {
+			this.broadcastQueue();
+		}.bind(this)); //without the bind, this refers to the event object that emitted
+
+		//where clause:
+
+		function onConnect(soc) {
+			const id = WebSocketHandler.socToUserId(soc);
+
+			//send queue
+			this.sendQueue(soc);
+
+			//save user
+			UserRecServ.add(id);
+			UserRecServ.setWS(id, soc);
+
+			//notify if banned
+			if (UserRecServ.isBanned(id)) this.sendBanned(soc);
+
+			//tell user their nickname
+			if (UserRecServ.isUser(id)) this.sendNickname(soc, UserRecServ.getNickname(id));
+		}
+
+		function onMessage(soc, data) {
+			const dataObj = JSON.parse(data);
+
+			if (dataObj.type === 'delete-content') {
+				if (!ContentServer.remove(this.wsh.socToUserId(soc), dataObj.contentId)) {
+					soc.send(JSON.stringify({
+						type: dataObj.type,
+						success: false,
+						reason: 'The queue item you tried to remove was not chosen by you.',
+					}));
+				}
+
+			} else {
+				soc.send(JSON.stringify({
+					type: dataObj.type,
+					success: false,
+					reason: 'The server did not recognise the type of message you were trying to send.',
+				}));
+			}
+		}
+
+		function onClose(soc, id) {
+			UserRecServ.unsetWS(id, soc);
+		}
+	}
+
 	//message related
 
-	static sendMessage(socs, type, mes) {
+	sendMessage(socs, type, mes) {
 		if (!socs) {
 			debug.log('no socs given');
 			debug.trace();
@@ -67,14 +74,14 @@ class Api {
 			message: mes,
 		});
 
-		wsHandler.sendToMany(socs, message);
+		this.wsh.sendToMany(socs, message);
 	}
 
-	static sendNickname(soc, nickname) {
+	sendNickname(soc, nickname) {
 		this.sendMessage(soc, 'nickname', nickname);
 	}
 
-	static sendBanned(socs) {
+	sendBanned(socs) {
 		if (!socs) {
 			debug.log('no socs given');
 			debug.trace();
@@ -87,10 +94,10 @@ class Api {
 			banned: true,
 		});
 
-		wsHandler.sendToMany(socs, message);
+		this.wsh.sendToMany(socs, message);
 	}
 		
-	static sendError(socs, type, reason) {
+	sendError(socs, type, reason) {
 		if (!socs) {
 			debug.log('no socs given');
 			debug.trace();
@@ -103,21 +110,21 @@ class Api {
 			message: reason,
 		});
 
-		wsHandler.sendToMany(socs, message);
+		this.wsh.sendToMany(socs, message);
 	}
 
-	static broadcastMessage(type, mes) {
+	broadcastMessage(type, mes) {
 		const message = JSON.stringify({
 			type: type,
 			message: mes,
 		});
 
-		wsHandler.broadcast(message);
+		this.wsh.broadcast(message);
 	}
 
 	//queue related
 
-	static makeQueueMessage() {
+	makeQueueMessage() {
 		return {
 			type: 'queue',
 			current: ContentServer.getCurrentlyPlaying(),
@@ -125,7 +132,7 @@ class Api {
 		};
 	}
 
-	static sendQueue(socs) {
+	sendQueue(socs) {
 		if (!socs) {
 			debug.log('no socs given');
 			debug.trace();
@@ -134,12 +141,12 @@ class Api {
 
 		const message = JSON.stringify(this.makeQueueMessage());
 
-		wsHandler.sendToMany(socs, message);
+		this.wsh.sendToMany(socs, message);
 	}
 
-	static broadcastQueue() {
-		this.broadcast(JSON.stringify(this.makeQueueMessage()));
+	broadcastQueue() {
+		this.wsh.broadcast(JSON.stringify(this.makeQueueMessage()));
 	}
 }
 
-module.exports = Api;
+module.exports = new Api();
