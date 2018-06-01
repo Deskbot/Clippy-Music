964a000cba96056bf59b2f74509cbb211dac8ad0
Thomas Richards <thomas.richards.96@gmail.com>
2018-06-01 13:00:37 +0100

    Spring Cleaning

--- serv/HttpServer.js
+++ serv/HttpServer.js
@@ -44,142 +44,6 @@ function getFormMiddleware(req, res, next) {
 	});
 }
 
-function recordUserMiddleware(req, res, next) {
-	if (!UserRecordServer.isUser(req.ip)) UserRecordServer.add(req.ip);
-
-	const expiryDate = new Date();
-	expiryDate.setYear(expiryDate.getYear() + 1901);
-
-	//store user id in cookie
-	res.cookie('id', req.ip, {
-		encode: a => a,
-		expires: expiryDate,
-	});
-
-	next();
-}
-
-const app = express();
-
-app.use('/', express.static(__dirname + '/../static/'));
-
-app.get('/api/wsport', (req, res) => {
-	res.status(200).end(opt.webSocketPort.toString());
-})
-
-/* Post variables:
-	* music-file (file)
-	* music-url
-	* image-file (file)
-	* image-url
-	* start-time
-	* end-time
- */
-app.post('/api/content/upload', recordUserMiddleware, (req, res) => {
-	Promise.resolve(handlePotentialBan(req.ip)) //assumes ip address is userId
-	.then(() => getFileForm(req))
-	.then(utils.spread((form, fields, files) => { //nesting in order to get the scoping right
-		return parseForm(form, fields, files)
-		.then((uplData) => {
-			uplData.userId = req.ip;
-			return ContentServer.add(uplData); //ContentServer.add would lose "this" keyword if passed as a function instead of within a lambda
-		})
-		.then(() => {
-			if (fields.ajax || req.headers['user-agent'].includes('curl'))
-				res.status(200).end('Success\n');
-			else
-				res.redirect('/');
-		});
-	}))
-	.catch((err) => {
-		if (err instanceof FileUploadError) {
-			res.status(400).end(err.message);
-		}
-		else if (err instanceof BannedError) {
-			res.status(400).end('You can not upload content because you are banned.');
-		}
-		else if (err instanceof YTError) {
-			res.status(400).end(err.message);
-		}
-		else {
-			console.error('Unknown upload error: ', err);
-			res.status(500).end(err.message);
-		}
-	});
-});
-
-app.use(getFormMiddleware);
-
-//POST variable: content-id
-app.post('/api/content/remove', (req, res) => {
-	if (!ContentServer.remove(req.ip, parseInt(req.fields['content-id']))) {
-		res.status(400).end('The queue item you tried to remove was not chosen by you.');
-	} else {
-		if (noRedirect(req)) res.status(200).end('Success\n');
-		else                 res.redirect('/');
-	}
-});
-
-//POST variable: nickname
-app.post('/api/nickname/set', recordUserMiddleware, (req, res) => {
-	UserRecordServer.setNickname(req.ip, Html5Entities.encode(req.fields.nickname.substr(0, opt.nicknameSizeLimit)));
-	
-	if (noRedirect(req)) res.status(200).end('Success\n');
-	else                 res.redirect('/');
-});
-
-//POST variable: password, id
-app.post('/api/ban/add', (req, res) => {
-	if (PasswordServer.verify(req.fields.password)) {
-		if (UserRecordServer.isUser(req.fields.id)) {
-			UserRecordServer.addBan(req.fields.id);
-			ContentServer.purgeUser(req.fields.id);
-			if (noRedirect(req)) res.status(200).end('Success\n');
-			else                 res.redirect('/');
-
-		} else {
-			res.status(400).end('That user doesn\'t exist.\n');
-		}
-		
-	} else {
-		res.status(400).end('Admin password incorrect.\n');
-	}
-});
-
-//POST variable: password, id
-app.post('/api/ban/remove', (req, res) => {
-	if (PasswordServer.verify(req.fields.password)) {
-		if (UserRecordServer.isBanned(req.fields.id)) {
-			UserRecordServer.removeBan(req.fields.id);
-			if (noRedirect(req)) res.status(200).end('Success\n');
-			else                 res.redirect('/');
-
-		} else {
-			res.status(400).end('That user is not banned.\n');
-		}
-	} else {
-		res.status(400).end('Admin password incorrect.\n');
-	}
-});
-
-//POST variable: password
-app.post('/api/content/kill', (req, res) => {
-	if (PasswordServer.verify(req.fields.password)) {
-		ContentServer.killCurrent();
-		res.status(200).end('Success\n');
-	} else {
-		res.status(400).end('Admin password incorrect.\n');
-	}
-});
-
-app.listen(opt.httpPort, (err) => {
-	if (err) throw err;
-
-	console.log('Web server started');
-});
-
-//functions (I'd like to nest them in other functions like a where clause but the function will be reallocated each time)
-
 function handlePotentialBan(userId) {
 	return new Promise((resolve, reject) => {
 		if (UserRecordServer.isBanned(userId)) {
@@ -308,6 +172,142 @@ function parseForm(form, fields, files) {
 		throw err;
 	});
 }
+
+function recordUserMiddleware(req, res, next) {
+	if (!UserRecordServer.isUser(req.ip)) UserRecordServer.add(req.ip);
+
+	const expiryDate = new Date();
+	expiryDate.setYear(expiryDate.getYear() + 1901);
+
+	//store user id in cookie
+	res.cookie('id', req.ip, {
+		encode: a => a,
+		expires: expiryDate,
+	});
+
+	next();
+}
+
+//creation of express instance and attaching handlers
+
+const app = express();
+
+app.use('/', express.static(__dirname + '/../static/'));
+
+app.get('/api/wsport', (req, res) => {
+	res.status(200).end(opt.webSocketPort.toString());
+})
+
+/* Post variables:
+	* music-file (file)
+	* music-url
+	* image-file (file)
+	* image-url
+	* start-time
+	* end-time
+ */
+app.post('/api/content/upload', recordUserMiddleware, (req, res) => {
+	handlePotentialBan(req.ip) //assumes ip address is userId
+	.then(() => getFileForm(req))
+	.then(utils.spread((form, fields, files) => { //nesting in order to get the scoping right
+		return parseForm(form, fields, files)
+		.then((uplData) => {
+			uplData.userId = req.ip;
+			return ContentServer.add(uplData); //ContentServer.add would lose "this" keyword if passed as a function instead of within a lambda
+		})
+		.then(() => {
+			if (fields.ajax || req.headers['user-agent'].includes('curl'))
+				res.status(200).end('Success\n');
+			else
+				res.redirect('/');
+		});
+	}))
+	.catch((err) => {
+		if (err instanceof FileUploadError) {
+			res.status(400).end(err.message);
+		}
+		else if (err instanceof BannedError) {
+			res.status(400).end('You can not upload content because you are banned.');
+		}
+		else if (err instanceof YTError) {
+			res.status(400).end(err.message);
+		}
+		else {
+			console.error('Unknown upload error: ', err);
+			res.status(500).end(err.message);
+		}
+	});
+});
+
+app.use(getFormMiddleware);
+
+//POST variable: content-id
+app.post('/api/content/remove', (req, res) => {
+	if (!ContentServer.remove(req.ip, parseInt(req.fields['content-id']))) {
+		res.status(400).end('The queue item you tried to remove was not chosen by you.');
+	} else {
+		if (noRedirect(req)) res.status(200).end('Success\n');
+		else                 res.redirect('/');
+	}
+});
+
+//POST variable: nickname
+app.post('/api/nickname/set', recordUserMiddleware, (req, res) => {
+	UserRecordServer.setNickname(req.ip, Html5Entities.encode(req.fields.nickname.substr(0, opt.nicknameSizeLimit)));
+	
+	if (noRedirect(req)) res.status(200).end('Success\n');
+	else                 res.redirect('/');
+});
+
+//POST variable: password, id
+app.post('/api/ban/add', (req, res) => {
+	if (PasswordServer.verify(req.fields.password)) {
+		if (UserRecordServer.isUser(req.fields.id)) {
+			UserRecordServer.addBan(req.fields.id);
+			ContentServer.purgeUser(req.fields.id);
+			if (noRedirect(req)) res.status(200).end('Success\n');
+			else                 res.redirect('/');
+
+		} else {
+			res.status(400).end('That user doesn\'t exist.\n');
+		}
+		
+	} else {
+		res.status(400).end('Admin password incorrect.\n');
+	}
+});
+
+//POST variable: password, id
+app.post('/api/ban/remove', (req, res) => {
+	if (PasswordServer.verify(req.fields.password)) {
+		if (UserRecordServer.isBanned(req.fields.id)) {
+			UserRecordServer.removeBan(req.fields.id);
+			if (noRedirect(req)) res.status(200).end('Success\n');
+			else                 res.redirect('/');
+
+		} else {
+			res.status(400).end('That user is not banned.\n');
+		}
+	} else {
+		res.status(400).end('Admin password incorrect.\n');
+	}
+});
+
+//POST variable: password
+app.post('/api/content/kill', (req, res) => {
+	if (PasswordServer.verify(req.fields.password)) {
+		ContentServer.killCurrent();
+		res.status(200).end('Success\n');
+	} else {
+		res.status(400).end('Admin password incorrect.\n');
+	}
+});
+
+app.listen(opt.httpPort, (err) => {
+	if (err) throw err;
+
+	console.log('Web server started');
+});
 
 //error classes
 
