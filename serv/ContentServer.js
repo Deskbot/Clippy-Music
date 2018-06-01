6f6818c57a0ebf683adfe95c57a48a9a8d161072
Thomas Richards <thomas.richards.96@gmail.com>
2018-06-01 20:04:54 +0100

    Fixes a roadblock for playing content
    Also adds error handling there

--- serv/ContentServer.js
+++ serv/ContentServer.js
@@ -2,13 +2,19 @@ const q = require('q');
 
 const ContentManager = require('../lib/ContentManager.js');
 
+const utils = require('../lib/utils.js');
+
 const cm = new ContentManager(ContentManager.recover());
 
 //set up
 function play() {
 	const isNext = cm.playNext();
 
-	if (!isNext) q.delay(1000).then(play);
+	if (!isNext) {
+		q.delay(1000)
+		.then(play)
+		.catch(utils.reportError);
+	}
 }
 
 cm.on('end', play);
