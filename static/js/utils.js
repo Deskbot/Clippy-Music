333676fc860021d2c1c58ae501214bc427c92ca9
Thomas Richards <thomas.richards.96@gmail.com>
2018-06-10 19:40:32 +0100

    Shifts sections to undo movement from DOM positioning
    In cases where a section is removed or resized by any user interaction or web socket event.

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,10 +1,38 @@
 var utils = {
+	counterShiftResize: function($elem, procedure) {
+		var oldHeight = utils.fullHeight($elem);
+
+		var ret = procedure();
+
+		var newHeight = utils.fullHeight($elem);
+
+		//counter move later windows unless neither have been moved
+		//in which case we want the natural page flow
+		utils.shiftDownElemsBelow($elem, oldHeight - newHeight, function($e) {
+			return utils.elemIsMoved($elem) || utils.elemIsMoved($e);
+		});
+
+		return ret;
+	},
+
 	displayNickname: function(name) {
 		$('.my-nickname').text(name);
 	},
 
+	elemIsMoved: function($elem) {
+		return $elem.attr('data-moved');
+	},
+
 	entitle: function(title) {
 		return '"' + utils.htmlEntityDecode(title) + '"';
+	},
+
+	fullHeight: function($elem) {
+		return $elem.height() + utils.removePx($elem.css('margin-top')) + utils.removePx($elem.css('margin-bottom'));
+	},
+
+	getUrlStart: function() {
+		return window.location.protocol + '//' + window.location.hostname;
 	},
 
 	htmlEntityDecode: function(str) {
@@ -21,7 +49,21 @@ var utils = {
 			|| url.includes('youtu.be');
 	},
 
-	getUrlStart: function getUrlStart() {
-		return window.location.protocol + '//' + window.location.hostname;
+	removePx: function(str) {
+		str = str.replace('px', '');
+		return parseInt(str);
+	},
+
+	shiftDownElemsBelow: function($elem, distance, condition) {
+		//only shift the later siblings of elem
+		var $laterSections = $elem.nextAll();
+		
+		for (var i = 0; i < $laterSections.length; i++) {
+			var $s = $($laterSections[i]);
+
+			if (condition && !condition($s)) continue;
+
+			$s.css('top', utils.removePx($s.css('top')) + distance + 'px');
+		}
 	}
 };
