2eaccc7a459373dd710ac8968498697b11451a36
Thomas Richards <thomas.richards.96@gmail.com>
2018-06-17 16:32:57 +0100

    Read percentage downloaded from youtube-dl process

--- options.js
+++ options.js
@@ -8,13 +8,15 @@ module.exports = {
 	nicknameSizeLimit: 33,
 	uniqueCoolOff: 86400,
 	streamYtOverDur: 1200,
+	dlPercentUpdateFreq: 2000,
 };
 
-//password          = the admin password, null gives anyone access
-//timeout           = the maximum number of seconds content can play for
-//storageDir        = the location on disk where uploaded content is stored, if a relative path is given, the directory will be relative to where the process was ran
-//imageSizeLimit    = largest image that can be downloaded locally or from the internet, in bytes
-//musicSizeLimit    = largest image that can be downloaded locally, in bytes
-//nicknameSizeLimit = maximum nickname length
-//uniquenessCoolOff = how many seconds before it's ok to play the same music or show the same picture again. Can use Infinity for nothing to ever be played twice, or 0 to never check for uniqueness
-//streamYtOverDur   = videos longer than this in seconds will be streamed from YouTube not downloaded first. (Long videos can take a while to download, even though a small fragment of them may be played; streaming a video causes a small delay between content.)
+//password            = the admin password, null gives anyone access
+//timeout             = the maximum number of seconds content can play for
+//storageDir          = the location on disk where uploaded content is stored, if a relative path is given, the directory will be relative to where the process was ran
+//imageSizeLimit      = largest image that can be downloaded locally or from the internet, in bytes
+//musicSizeLimit      = largest image that can be downloaded locally, in bytes
+//nicknameSizeLimit   = maximum nickname length
+//uniquenessCoolOff   = how many seconds before it's ok to play the same music or show the same picture again. Can use Infinity for nothing to ever be played twice, or 0 to never check for uniqueness
+//streamYtOverDur     = videos longer than this in seconds will be streamed from YouTube not downloaded first. (Long videos can take a while to download, even though a small fragment of them may be played; streaming a video causes a small delay between content.)
+//dlPercentUpdateFreq = how many milliseconds between updating the download percentage for videos on the front end
