c9ba6d33627375487ffa4d6028d6294b24020b7f
Thomas Richards <thomas.richards.96@gmail.com>
2018-07-01 16:46:26 +0100

    Adds youtube-dl path option

--- options.js
+++ options.js
@@ -1,22 +1,26 @@
 module.exports = {
-	httpPort: 80,
+	httpPort: 8080,
 	webSocketPort: 3000,
+
+	dlPercentUpdateFreq: 2000,
+	streamYtOverDur: 1200,
 	timeout: 644,
+	uniqueCoolOff: 86400,
+
 	storageDir: '/tmp/Clippy-Music-Tmp',
+	youtubeDlPath: 'youtube-dl',
+	
 	imageSizeLimit: 100000000,
 	musicSizeLimit: 400000000,
 	nicknameSizeLimit: 33,
-	uniqueCoolOff: 86400,
-	streamYtOverDur: 1200,
-	dlPercentUpdateFreq: 2000,
+	
 };
 
-//password            = the admin password, null gives anyone access
-//timeout             = the maximum number of seconds content can play for
-//storageDir          = the location on disk where uploaded content is stored, if a relative path is given, the directory will be relative to where the process was ran
+//dlPercentUpdateFreq = how many milliseconds between updating the download percentage for videos on the front end
 //imageSizeLimit      = largest image that can be downloaded locally or from the internet, in bytes
 //musicSizeLimit      = largest image that can be downloaded locally, in bytes
 //nicknameSizeLimit   = maximum nickname length
-//uniquenessCoolOff   = how many seconds before it's ok to play the same music or show the same picture again. Can use Infinity for nothing to ever be played twice, or 0 to never check for uniqueness
+//storageDir          = the location on disk where uploaded content is stored, if a relative path is given, the directory will be relative to where the process was ran
 //streamYtOverDur     = videos longer than this in seconds will be streamed from YouTube not downloaded first. (Long videos can take a while to download, even though a small fragment of them may be played; streaming a video causes a small delay between content.)
-//dlPercentUpdateFreq = how many milliseconds between updating the download percentage for videos on the front end
+//timeout             = the maximum number of seconds content can play for
+//uniquenessCoolOff   = how many seconds before it's ok to play the same music or show the same picture again. Can use Infinity for nothing to ever be played twice, or 0 to never check for uniqueness
