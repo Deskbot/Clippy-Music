47652fd32c04aedc9c4262e36a7ec460d5f33f99
Thomas Richards <thomas.richards.96@gmail.com>
2018-12-19 13:16:56 +0000

    Add tests  to ip address identifier

--- test/src/lib/utils.js
+++ test/src/lib/utils.js
@@ -5,16 +5,44 @@ const assert = require("assert").strict;
 
 module.exports = {
     looksLikeIpAddress: () => {
+        // ipv4
+
         assert(utils.looksLikeIpAddress("0.0.0"));
         assert(utils.looksLikeIpAddress("12.12.12"));
         assert(utils.looksLikeIpAddress("123.123.123"));
         assert(utils.looksLikeIpAddress("0.0.0.0"));
         assert(utils.looksLikeIpAddress("12.12.12.12"));
         assert(utils.looksLikeIpAddress("123.123.123.123"));
+        assert(utils.looksLikeIpAddress("123.123.123.123.123"));
+        assert(utils.looksLikeIpAddress("  12.12.12.12\t"));
 
         assert.strictEqual(utils.looksLikeIpAddress("0.0"), false);
         assert.strictEqual(utils.looksLikeIpAddress("hello 0.0.0.0"), false);
         assert.strictEqual(utils.looksLikeIpAddress("0.0.0.0 hello"), false);
         assert.strictEqual(utils.looksLikeIpAddress("hello 0.0.0.0 hello"), false);
+
+        //ipv6
+
+        assert(utils.looksLikeIpAddress("ffff:ffff:ffff"));
+        assert(utils.looksLikeIpAddress("ffff:ffff:ffff:ffff"));
+        assert(utils.looksLikeIpAddress("ffff:ffff::ffff"));
+        assert(utils.looksLikeIpAddress("ffff:ffff::::ffff"));
+        assert(utils.looksLikeIpAddress("0000:0000:0000:0000:0000:0000:0000:0000"));
+        assert(utils.looksLikeIpAddress("::ffff::"));
+        assert(utils.looksLikeIpAddress("\tffff:ffff:ffff:ffff  "));
+
+        assert.strictEqual(utils.looksLikeIpAddress("hello ffff:ffff:ffff:ffff"), false);
+        assert.strictEqual(utils.looksLikeIpAddress("ffff:ffff:ffff:ffff hello"), false);
+        assert.strictEqual(utils.looksLikeIpAddress("hello ffff:ffff:ffff:ffff hello"), false);
+        assert.strictEqual(utils.looksLikeIpAddress("xyz:ffff:ffff:ffff"), false);
+        assert.strictEqual(utils.looksLikeIpAddress("ffff.ffff.ffff.ffff"), false);
+        assert.strictEqual(utils.looksLikeIpAddress("fff:fffffff:fff"), false);
+        assert.strictEqual(utils.looksLikeIpAddress("abc:abc"), false);
+
+        //ipv6 followed by ipv4
+
+        assert(utils.looksLikeIpAddress("::127.0.0.1"));
+        assert(utils.looksLikeIpAddress(":ffff::127.0.0.1"));
+        assert(utils.looksLikeIpAddress(":ffff:1234:6564:45:127.0.0.1"));
     },
 };
