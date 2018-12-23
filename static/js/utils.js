7101d95aa12735d91ae5cd9c5f805b7d6a0b0adb
Thomas Richards <thomas.richards.96@gmail.com>
2018-12-23 14:35:51 +0000

    Don't account for movement of noscript tags

--- static/js/utils.js
+++ static/js/utils.js
@@ -60,7 +60,7 @@ var utils = {
 
 	shiftDownElemsBelow: function($elem, distance, condition) {
 		//only shift the later siblings of elem
-		var $laterSections = $elem.nextAll();
+		var $laterSections = $elem.nextAll('section'); // don't include noscript tags
 
 		for (var i = 0; i < $laterSections.length; i++) {
 			var $s = $($laterSections[i]);
