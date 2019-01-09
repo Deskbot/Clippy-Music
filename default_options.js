64995a322cde67adcdcd0a8d47e80fc2af010588
Deskbot <Deskbot@users.noreply.github.com>
2019-01-09 20:27:31 +0000

    Reorder deafult_options and add comments for attributes

--- default_options.js
+++ default_options.js
@@ -8,6 +8,7 @@ module.exports = {
 	timeout: 644,
 
 	storageDir: '/tmp/Clippy-Music-Tmp',
+
 	mpvPath: 'mpv',
 	mpvArgs: ['-vo', 'xv', '-fs', '--af=drc=2:0.25'],
 	youtubeDlPath: 'youtube-dl',
@@ -15,15 +16,17 @@ module.exports = {
 	dlPercentUpdateFreq: 2000,
 	imageSizeLimit: 100000000,
 	musicSizeLimit: 400000000,
-	nicknameSizeLimit: 33,
-	fileNameSizeLimit: 33,
+	nicknameSizeLimit: 67,
+	fileNameSizeLimit: 57,
 };
 
-//dlPercentUpdateFreq = how many milliseconds between updating the download percentage for videos on the front end
-//imageSizeLimit      = largest image that can be downloaded locally or from the internet, in bytes
-//musicSizeLimit      = largest image that can be downloaded locally, in bytes
-//nicknameSizeLimit   = maximum nickname length
-//storageDir          = the location on disk where uploaded content is stored, if a relative path is given, the directory will be relative to where the process was ran
+//imageUniqueCoolOff  = how many seconds before it's ok to play the same music again. Can use Infinity for nothing to ever be played twice, or 0 to never check for uniqueness
+//musicUniqueCoolOff  = how many seconds before it's ok to show the same picture again. Can use Infinity for nothing to ever be played twice, or 0 to never check for uniqueness
 //streamYtOverDur     = videos longer than this in seconds will be streamed from YouTube not downloaded first. (Long videos can take a while to download, even though a small fragment of them may be played; streaming a video causes a small delay between content.)
 //timeout             = the maximum number of seconds content can play for
-//uniquenessCoolOff   = how many seconds before it's ok to play the same music or show the same picture again. Can use Infinity for nothing to ever be played twice, or 0 to never check for uniqueness
+//storageDir          = the location on disk where uploaded content is stored, if a relative path is given, the directory will be relative to where the process was ran
+//dlPercentUpdateFreq = how many milliseconds between updating the download percentage for videos on the front end
+//imageSizeLimit      = largest image file that can be downloaded, in bytes
+//musicSizeLimit      = largest music file that can be downloaded, in bytes
+//nicknameSizeLimit   = maximum nickname length
+//fileNameSizeLimit   = number of file name characters shown on front end
