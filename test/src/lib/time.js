7dce5cd9911040139296388484c909b2f7146c06
Thomas Richards <Deskbot@users.noreply.github.com>
2019-07-25 20:31:52 +0100

    Add function for calculating intended duration

---
+++ test/src/lib/time.js
@@ -0,0 +1,20 @@
+const baseDir = "../../../";
+const time = require(baseDir + "lib/time.js");
+
+const assert = require("assert").strict;
+
+module.exports = {
+    clipTimeByStartAndEnd: () => {
+        assert.equal(time.clipTimeByStartAndEnd(0, 0, 0), 0);
+        assert.equal(time.clipTimeByStartAndEnd(100, 0, 0), 0);
+        assert.equal(time.clipTimeByStartAndEnd(100, 10, 10), 0);
+        assert.equal(time.clipTimeByStartAndEnd(100, 0, 10), 10);
+        assert.equal(time.clipTimeByStartAndEnd(100, 20, 80), 60);
+
+        assert.equal(time.clipTimeByStartAndEnd(100, null, null), 100);
+        assert.equal(time.clipTimeByStartAndEnd(100, null, 0), 0);
+        assert.equal(time.clipTimeByStartAndEnd(100, 0, null), 100);
+        assert.equal(time.clipTimeByStartAndEnd(100, null, 20), 20);
+        assert.equal(time.clipTimeByStartAndEnd(100, 20, null), 80);
+    },
+};
