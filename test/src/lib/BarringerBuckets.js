ee161711623bcb434078318370325a5655e726ce
Thomas Richards <Deskbot@users.noreply.github.com>
2019-07-25 20:31:42 +0100

    Can't add to the top bucket

--- test/src/lib/BarringerBuckets.js
+++ test/src/lib/BarringerBuckets.js
@@ -5,7 +5,7 @@ const BarringerBuckets = require(baseDir + "lib/BarringerBuckets.js");
 const assert = require("assert").strict;
 
 module.exports = {
-    can_add_item: () => {
+    can_add_to_empty_queue: () => {
         const q = new BarringerBuckets(1000);
 
         const item = {
@@ -14,7 +14,24 @@ module.exports = {
         };
         q.add(item);
 
-        assert(q.getBuckets()[0][0] === item, "The added item is now in the queue.");
+        assert(q.getBuckets()[0][0] === item, "The added item is in the top bucket.");
+    },
+
+    can_not_add_to_top_bucket_if_queue_is_not_empty: () => {
+        const q = new BarringerBuckets(1000);
+
+        q.add({
+            userId: 2,
+            duration: 2
+        });
+
+        const item = {
+            userId: 1,
+            duration: 10,
+        };
+        q.add(item);
+
+        assert(q.getBuckets()[1][0] === item, "The added item is in the second bucket.");
     },
 
     exceeding_a_bucket_size_adds_a_new_bucket: () => {
