14deaf2fb18cb703616d2e733e305206b2235756
Thomas Richards <Deskbot@users.noreply.github.com>
2019-08-18 14:49:03 +0100

    Reformat some lines

--- src-server/lib/consts.ts
+++ src-server/lib/consts.ts
@@ -2,12 +2,18 @@ import * as opt from '../../options';
 import * as path from "path";
 import * as utils from './utils';
 
-export const biggestFileSizeLimit = opt.musicSizeLimit > opt.imageSizeLimit ? opt.musicSizeLimit : opt.imageSizeLimit;
+export const biggestFileSizeLimit = opt.musicSizeLimit > opt.imageSizeLimit
+	? opt.musicSizeLimit
+	: opt.imageSizeLimit;
 
 export const imageSizeLimStr = utils.sizeToReadbleStr(opt.imageSizeLimit);
 export const musicSizeLimStr = utils.sizeToReadbleStr(opt.musicSizeLimit);
-export const imagePlayedWithin = opt.imageUniqueCoolOff === Infinity ? 'already' : 'in the past ' + utils.secToTimeStr(opt.imageUniqueCoolOff);
-export const musicPlayedWithin = opt.musicUniqueCoolOff === Infinity ? 'already' : 'in the past ' + utils.secToTimeStr(opt.musicUniqueCoolOff);
+export const imagePlayedWithin = opt.imageUniqueCoolOff === Infinity
+	? 'already'
+	: 'in the past ' + utils.secToTimeStr(opt.imageUniqueCoolOff);
+export const musicPlayedWithin = opt.musicUniqueCoolOff === Infinity
+	? 'already'
+	: 'in the past ' + utils.secToTimeStr(opt.musicUniqueCoolOff);
 
 export const maxPercentBeforeFinished = 0.99;
 export const minPlayTimeToPreventReplay = 5; //seconds
