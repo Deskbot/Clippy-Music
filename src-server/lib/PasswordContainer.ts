b9d629d99c5c328d4e1db0f1180781ca08e91280
Deskbot <Deskbot@users.noreply.github.com>
2019-10-05 19:00:21 +0100

    Use some node crypto library functions

--- src-server/lib/PasswordContainer.ts
+++ src-server/lib/PasswordContainer.ts
@@ -1,39 +1,23 @@
-/*
- * I'm generating the salt and hashing it myself because bcrypt has tons of dependencies,
- * the one password will only ever be in memory,
- * the password will only be generated once.
- */
 import * as crypto from "crypto";
 
 export class PasswordContainer {
-	private salt: string;
-	private password: string;
+	private salt: Buffer;
+	private hashedPassword: Buffer;
 
 	constructor(inputPass: string) {
-		this.salt = PasswordContainer.newSalt(32);
-		this.password = hash(inputPass + this.salt);
+		this.salt = newSalt();
+		this.hashedPassword = hash(inputPass, this.salt);
 	}
 
 	verify(inputPass: string) {
-		return hash(inputPass + this.salt) === this.password;
-	}
-
-	static newSalt(len: number) {
-		let str = "";
-
-		//8 is the number of chars added to str each time
-		for (let i = 0; i < len; i += 8) {
-			//add a random string
-			str += Math.random().toString(36).substr(2);
-		}
-
-		return str.substr(len);
+		return hash(inputPass, this.salt) === this.hashedPassword;
 	}
 }
 
-function hash(data: string): string {
-	return crypto.createHash("sha256")
-		.update(data)
-		.digest()
-		.toString();
+function hash(password: string, salt: Buffer): Buffer {
+	return crypto.scryptSync(password, salt, 64);
+}
+
+function newSalt(): Buffer {
+	return crypto.randomBytes(64);
 }
