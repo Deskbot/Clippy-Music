c95dbe4ac2cbd019dcfe20b6917f0a5aa6317002
Thomas Richards <Deskbot@users.noreply.github.com>
2020-03-08 14:55:39 +0000

    Add comment

--- src-server/lib/ContentManager.ts
+++ src-server/lib/ContentManager.ts
@@ -326,8 +326,23 @@ export class ContentManager extends EventEmitter {
 		if (contentData.pic.exists) {
 			const picPath = contentData.pic.path;
 			const showPicture = (buf: any) => {
-				//we want to play the picture after the video has appeared, which takes a long time when doing it remotely
-				//so we have to check the output of mpv, for signs it's not just started up, but also playing :/
+				// we want to play the picture after the video has appeared, which takes a long time when the video is remote
+				// so we have to check the output of the mpv process for when it creates a window
+
+				// Example mpv output. The lines beginning with square brackets only appear with `--msg-level=all=v`, which Clippy is not using.
+				// ...
+				// (+) Video --vid=1 (*) (h264 1920x1080 60.000fps)
+                // (+) Audio --aid=1 --alang=eng (*) (opus 2ch 48000Hz)
+				// [vo/gpu] Probing for best GPU context.
+				// [vo/gpu/opengl] Initializing GPU context 'wayland'
+				// [vo/gpu/opengl] Initializing GPU context 'x11egl'
+				// [vo/gpu/x11] X11 opening display: :0
+				// [vo/gpu/x11] X11 running at 2560x1440 (":0" => local display)
+				// ...
+				// AO: [pulse] 48000Hz stereo 2ch float
+				// ...
+				// VO: [gpu] 1920x1080 yuv420p
+				// ...
 				if (buf.includes("AO") || buf.includes("VO")) {
 					this.startPic(picPath, opt.timeout);
 					musicProc.stdout.removeListener("data", showPicture); //make sure we only check for this once, for efficiency
