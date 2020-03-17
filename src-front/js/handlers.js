4e8b0b27d87bd6fd17775ff6a19067dac4296a21
Thomas Richards <Deskbot@users.noreply.github.com>
2020-03-17 16:30:19 +0000

    Remove invalid data type in jquery

--- src-front/js/handlers.js
+++ src-front/js/handlers.js
@@ -318,8 +318,7 @@ $("#dl-list-container").on("click", "button.cancel", function(e) {
 		data: {
 			ajax: true,
 			"content-id": contentId,
-		},
-		dataType: "text/plain"
+		}
 
 	}).done(function() {
 		main.clippyAgent.speak("The download of " + utils.entitle(contentName) + " was cancelled.");
